ae7c56948483591238c1ec5b30bdab11296572ff
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-04-15 11:51:13 -0500

    Add sketch.js for word manipulation and display

---
+++ sketch.js
@@ -0,0 +1,33 @@
+//Global variables
+let myInput, myButton, myImage,theirWord;
+
+function getWordLength(thisWord){
+  //thisWord has a scope local to this function only.
+  //we often use the word 'this' to show it's local
+  return thisWord.length
+}
+function getWord(){//click event of the button.
+  theirWord = myInput.value();//get whatever is in the inputBox
+  console.log(theirWord)
+}
+// P5JS functions  don't erase ----------------------------
+function setup() {
+  createCanvas(400, 400);
+  theirWord = "";
+  myInput = createInput("enter a word")
+  myInput.position(20,100)
+  myButton = createButton("Click this")
+  myButton.position(200,100)
+  myButton.mousePressed(getWord)
+  myImage = loadImage("Screenshot_2026-04-15_112449-removebg-preview.png")
+}
+function draw() {
+  background(220);
+  text("working with strings", 20,50)//title
+  text("All Caps: ", 20,150)
+  text("Length: " + getWordLength(theirWord), 20,175)
+  text("All Lower: ", 20,200)
+  text("List of vowels: ", 20,225)
+  image(myImage,200, 50)
+  
+}
